bf11ed80d83fd344b4e6681482801dd08adf33f8
Robert-Yaman <robert.e.yaman@gmail.com>
2015-10-07 18:46:30 -0700

    continue with organ

---
+++ Organ/app/assets/javascripts/util/note.js
@@ -0,0 +1,35 @@
+(function () {
+  var AudioContext = window.AudioContext || window.webkitAudioContext;
+  var ctx = new AudioContext();
+
+  var createOscillator = function (freq) {
+    var osc = ctx.createOscillator();
+    osc.type = "sine";
+    osc.frequency.value = freq;
+    osc.detune.value = 0;
+    osc.start(ctx.currentTime);
+    return osc;
+  };
+
+  var createGainNode = function () {
+    var gainNode = ctx.createGain();
+    gainNode.gain.value = 0;
+    gainNode.connect(ctx.destination);
+    return gainNode;
+  };
+
+  var Note = window.Note = function(freq) {
+    this.freq = freq;
+    this.oscillatorNode = createOscillator(freq);
+    this.gainNode = createGainNode();
+    this.oscillatorNode.connect(this.gainNode);
+ };
+
+  Note.prototype.start = function () {
+    this.gainNode.gain.value = 0.3;
+  };
+
+  Note.prototype.stop = function () {
+    this.gainNode.gain.value = 0;
+  };
+})();
---
+++ Organ/app/assets/javascripts/util/tones.js
@@ -0,0 +1,16 @@
+(function () {
+  var TONES = window.TONES = {
+    a : 440,
+    aSharp : 466.16,
+    b : 493.88,
+    c : 523.25,
+    cSharp : 554.37,
+    d : 587.33,
+    dSharp : 622.25,
+    e : 659.25,
+    f : 698.46,
+    fSharp : 739.99,
+    g : 783.99,
+    gSharp : 830.61
+  };
+})();
